e81d576492f21d84e29cd59721a006590f964e2d
hispanos <mailermartinez@gmail.com>
2021-06-01 10:33:00 -0500

    Get the form's cycling's data and save at object

--- scripts/Cycling.js
+++ scripts/Cycling.js
@@ -2,11 +2,31 @@ export default class Cycling {
 
     constructor() {
         this.form = document.getElementById('form-new-runner');
+        this.name = document.getElementById('name');
+        this.run1 = document.getElementById('run1');
+        this.run2 = document.getElementById('run2');
+        this.run3 = document.getElementById('run3');
+        this.run4 = document.getElementById('run4');
+        this.run5 = document.getElementById('run5');
+        this.runners = [];
     }
 
     saveCyclist(e) {
         e.preventDefault();
-        console.log('Hola')
+
+        const runner = {
+            name: this.name.value,
+            run1: this.run1.value,
+            run2: this.run2.value,
+            run3: this.run3.value,
+            run4: this.run4.value,
+            run5: this.run5.value,
+        }
+
+        this.runners.push(runner);
+        e.reset();
+
+        console.log(this.runners);
     }
 
 }
